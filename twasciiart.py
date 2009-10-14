21fdc9add0e4fc2aff1fdead174c5e2cede94d13
Wesley Mason <wes@toottoot.(none)>
2009-10-14 17:24:25 +0100

    Added in single tweet with carriage returns functionality

--- twasciiart.py
+++ twasciiart.py
@@ -7,7 +7,7 @@ import re
 import getopt
 
 def usage():
-    print >> sys.stdout """twasciiart.py accepts text sent via stdin, with optional arguments:
+        print >> sys.stdout """twasciiart.py accepts text sent via stdin, with optional arguments:
 echo "test" | ./twasciiart.py -u user42 -p somepass123 [-l -d]
 -u --username : twitter username
 -p --password : twitter password
@@ -21,68 +21,83 @@ to stuff input into a single tweet separated by carriage returns
 """
 
 def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "u:p:l:ll:d:t:n:r:", ["username=", "password=",
-            "length=", "linelength=", "delay=", "tag=", "linenumber=",
-            "replacer="])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    username = None
-    password = None
-    length = 140
-    line_length = 80
-    delay = 120
-    tag = ""
-    line_number = None
-    replacer = "."
-    for opt, arg in opts:
-        if opt in ("-u", "--username"):
-            username = arg
-        elif opt in ("-p", "--password"):
-            password = arg
-        elif opt in ("-l", "--length"):
-            length = arg
-        elif opt in ("-ll", "--linelength"):
-            length = arg
-        elif opt in ("-d", "--delay"):
-            delay = arg
-        elif opt in ("-t", "--tag"):
-            tag = arg
-        elif opt in ("-n", "--linenumber"):
-            line_number = arg
-        elif opt in ("-r", "--replacer"):
-            replacer = arg
-    if not username or not password:
-        print >> sys.stderr "username and password are required"
-        usage()
-        sys.exit(2)
-    tag = (" #%s" % tag) if tag else ""
-    length = length - len(tag)
-    tweets = []
-    tw = Api(username=username, password=password)
+        try:
+            opts, args = getopt.getopt(argv, "u:p:l:ll:d:t:n:r:", ["username=", "password=",
+                "length=", "linelength=", "delay=", "tag=", "linenumber=",
+                "replacer="])
+        except getopt.GetoptError:
+            usage()
+            sys.exit(2)
+        username = None
+        password = None
+        length = 140
+        line_length = 80
+        delay = 120
+        tag = ""
+        line_number = None
+        replacer = "."
+        for opt, arg in opts:
+            if opt in ("-u", "--username"):
+                username = arg
+            elif opt in ("-p", "--password"):
+                password = arg
+            elif opt in ("-l", "--length"):
+                length = arg
+            elif opt in ("-ll", "--linelength"):
+                length = arg
+            elif opt in ("-d", "--delay"):
+                delay = arg
+            elif opt in ("-t", "--tag"):
+                tag = arg
+            elif opt in ("-n", "--linenumber"):
+                line_number = arg
+            elif opt in ("-r", "--replacer"):
+                replacer = arg
+        if not username or not password:
+            print >> sys.stderr "username and password are required"
+            usage()
+            sys.exit(2)
+        tag = (" #%s" % tag) if tag else ""
+        length = length - len(tag)
+        tweets = []
+        tw = Api(username=username, password=password)
 
-    if not len(sys.stdin):
-        print >> sys.stderr "No input from stdin"
-        usage()
-        sys.exit(2)
+        if not len(sys.stdin):
+            print >> sys.stderr "No input from stdin"
+            usage()
+            sys.exit(2)
 
-    for l in sys.stdin.readlines():
-            line = e.sub(r'\s', replacer, line)
-            if len(line) > length:
-                    print >> sys.stderr "Lines too long to fit in a single tweet, change lines (or hashtag if present"
-                    usage()
-                    sys.exit(2)
-            else:
-                    tweets.append("%s%s" % (tag, line))
+        def format_line(line):
+            return e.sub(r'\s', replacer, line)
 
-    if tweets:
-            for i, tweet in enumerate(tweets):
-                    time.sleep(delay)
-                    print >> sys.stdout 'Sending tweet %d of %d: "%s"' % (i, len(tweets), tweet)
-                    tw.PostUpdate(tweet)
-                    print >> sys.stdout "Sent!"
-    print >> sys.stdout "Done."
+        if line_number:
+                if len(format_line(sys.stdin.read())) > length:
+                        print >> sys.stderr "Lines too long to fit in a single tweet, change lines (or hashtag if present)"
+                        usage()
+                        sys.exit(2)
+        for l in sys.stdin.readlines():
+                line = format_line(line)
+                if len(line) > length:
+                        print >> sys.stderr "Lines too long to fit in a single tweet, change lines (or hashtag if present)"
+                        usage()
+                        sys.exit(2)
+                else:
+                        if line_number:
+                                # Don't include tag, will be appended to end of
+                                # full tweet
+                                tweets.append(line)
+                        else:
+                                tweets.append("%s%s" % (tag, line))
+        if line_number:
+                tweets = ["%s%s" % ("\r".join(tweets), tag)]
+
+        if tweets:
+                for i, tweet in enumerate(tweets):
+                        time.sleep(delay)
+                        print >> sys.stdout 'Sending tweet %d of %d: "%s"' % (i, len(tweets), tweet)
+                        tw.PostUpdate(tweet)
+                        print >> sys.stdout "Sent!"
+        print >> sys.stdout "Done."
 
 if __name__ == "__main__":
-        main(sys.argv[1:])
+            main(sys.argv[1:])
